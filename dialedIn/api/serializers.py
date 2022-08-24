8e4a1188a68182fcd25c2dd5ab4c43ef61abaa87
Brandon Martinez <brandonam04@outlook.com>
2022-08-23 23:57:15 -0400

    Add endpoint for session authenticaion

--- dialedIn/api/serializers.py
+++ dialedIn/api/serializers.py
@@ -1,6 +1,6 @@
 from dialedIn.api.models import Extraction, Coffee
 from rest_framework import serializers
-
+from django.contrib.auth import authenticate
 
 class ExtractionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +12,32 @@ class CoffeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coffee
         fields = ['id','coffee_name', 'roaster_name', 'roast_level']
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        label="Username",
+        write_only=True
+    )
+
+    password = serializers.CharField(
+        label="Password",
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+        if username and password:
+            user = authenticate(request = self.context.get('request'), username = username, password = password)
+
+            if not user:
+                msg = 'Username or Password Incorrect'
+                raise serializers.ValidationError(msg, code = 'authorization')
+        else:
+            msg = 'Please input both username and password'
+            raise serializers.ValidationError(msg, code = 'authorization')
+        
+        attrs['user'] = user
+        return attrs
--- dialedIn/api/views.py
+++ dialedIn/api/views.py
@@ -1,7 +1,11 @@
 from dialedIn.api.models import Extraction, Coffee
 from rest_framework import viewsets
 from rest_framework import permissions
-from dialedIn.api.serializers import ExtractionSerializer, CoffeeSerializer
+from dialedIn.api.serializers import ExtractionSerializer, CoffeeSerializer, LoginSerializer
+from rest_framework.response import Response
+from rest_framework import views
+from django.contrib.auth import login
+from rest_framework import status
 
 
 class ExtractionViewSet(viewsets.ModelViewSet):
@@ -18,3 +22,12 @@ class CoffeeViewSet(viewsets.ModelViewSet):
     """
     queryset = Coffee.objects.all()
     serializer_class = CoffeeSerializer
+class LoginView(views.APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = LoginSerializer(data=self.request.data, context={'request':self.request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response(None, status=status.HTTP_202_ACCEPTED)
